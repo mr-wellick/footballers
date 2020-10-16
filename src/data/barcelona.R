ba7a04ad02e6fc9dd2f091774f66b9c2eceacdd2
Mr. Wellick <Lorenz3192@gmail.com>
2020-10-15 23:19:48 -0700

    Update barcelona.R

--- src/data/barcelona.R
+++ src/data/barcelona.R
@@ -3,13 +3,13 @@ library(dplyr)
 library(stringr)
 
 
-# construct base url's for atletico madrid seasons
+# construct base url's for barcelona seasons
 base_url     = 'https://www.espn.com/soccer/team/squad/_/id/83/league/ESP.1/season/'
 season_years = (2004:2020) %>% as.character()
 season_urls  = paste(base_url, sep="", season_years)
 season_list  = vector(mode = 'list', length = length((season_years)))
 
-# iterate through the years 2003:2020 and create a df for each year
+# iterate through the years 2004:2020 and create a dataframe for each year
 for (index in 1:length(season_urls)) {
   # extract table for specific season
   season_table = read_html(season_urls[index])
@@ -25,11 +25,11 @@ for (index in 1:length(season_urls)) {
   
   # change column names in df
   colnames(season_df) = tolower(col_names)
-  season_df['num'] = season_df$name %>% str_match('\\d+')
-  season_df$name = season_df$name %>% str_match('\\D+') %>% str_replace_all("[[:punct:]]", "")
+  season_df['num'] = season_df$name %>% str_match('\\d+') # extract player number and add new column to existing dataframe
+  season_df$name = season_df$name %>% str_match('\\D+') %>% str_replace_all("[[:punct:]]", "") # remove any special punctuation from player names to avoid issues when generating SQL commands
   
-  # add new data frame to season list
-  season_list[[index]] = season_df %>% select(name:age, nat:num)
+  # add new dataframe to season list
+  season_list[[index]] = season_df %>% select(name:age, nat:num) # drop unwanted columns before adding dataframe to season list
 }
 
 # save all seasons as csv's
