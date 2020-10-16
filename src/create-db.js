8fd0178d597dfa5a78fbb75313769bf9c7e304a7
mr-wellick <lorenz3192@gmail.com>
2020-10-15 21:55:59 -0700

    Update data

--- src/create-db.js
+++ src/create-db.js
@@ -1,9 +1,9 @@
-import fs from 'fs';
-import { promisify } from 'util';
-import { db } from './db.js';
-import path from 'path';
-import { csvParse } from 'd3-dsv';
-import chalk from 'chalk';
+const fs = require('fs');
+const { promisify } = require('util');
+const { db } = require('./db.js');
+const path = require('path');
+const { csvParse } = require('d3-dsv');
+const chalk = require('chalk');
 
 const query = promisify(db.query).bind(db);
 const readFile = promisify(fs.readFile);
@@ -12,32 +12,38 @@ const readDir = promisify(fs.readdir);
 const getFileNames = async () => {
   const files = await readDir(path.join(__dirname, '../src/data'));
   try {
-    return files;
+    const filteredFiles = files.filter(file => file.includes('csv'));
+    return filteredFiles;
   } catch (err) {
     return err;
   }
 };
 
-function isValid(str) {
-  return !/[~`!#$%^&*+=\-[\]\\';,/{}|\\":<>?]/g.test(str);
-}
-
 export const dropTable = async () => {
   const files = await getFileNames();
 
-  for (const file of files) {
-    const sql = `DROP TABLE IF EXISTS poc_config.season_${
-      file.split('.')[0]
-    };`;
-    console.log('');
-    console.log(chalk.cyan('---- DROPPING TABLE ----'));
-    console.log(sql);
-    query(sql);
+  try {
+    for (const file of files) {
+      const sql = `DROP TABLE IF EXISTS poc_config.season_${
+        file.split('.')[0]
+      };`;
+
+      console.log(
+        chalk.cyan('   DROPPING TABLE'),
+        ' ===> ',
+        chalk.magenta(`${sql}`)
+      );
+
+      query(sql);
+    }
+  } catch (err) {
+    console.error('Failed to drop tables', err);
   }
 };
 
 export const createTable = async () => {
   const files = await getFileNames();
+
   for (const file of files) {
     const contents = await readFile(
       path.join(__dirname, `../src/data/${file}`),
@@ -47,11 +53,10 @@ export const createTable = async () => {
     let rows = '';
 
     Object.keys(parsedData[0])
-      .filter(item => item !== '')
-      .filter(item => item !== 'HT')
-      .filter(item => item !== 'WT')
+      .filter(key => key !== 'num')
+      .filter(key => key !== '')
       .forEach(key => {
-        if (key === 'RC') {
+        if (key === 'rc') {
           rows += `${key} varchar(255)\n`;
         } else {
           rows += `${key} varchar(255),\n`;
@@ -79,42 +84,36 @@ export const insertIntoTable = async () => {
     );
     const parsedData = csvParse(contents);
     const keys = Object.keys(parsedData[0])
-      .filter(item => item !== '')
-      .filter(item => item !== 'WT')
-      .filter(item => item !== 'HT');
+      .filter(key => key !== 'num')
+      .filter(key => key !== '');
     let rowNames = '';
 
     Object.keys(parsedData[0])
-      .filter(item => item !== '')
-      .filter(item => item !== 'HT')
-      .filter(item => item !== 'WT')
+      .filter(key => key !== 'num')
+      .filter(key => key !== '')
       .forEach(key => {
-        if (key === 'RC') {
+        if (key === 'rc') {
           rowNames += `${key}`;
         } else {
           rowNames += `${key}, `;
         }
       });
-    console.log('');
+
     console.log(chalk.cyan('---- INSER INTO TABLE ----'));
     parsedData.forEach(item => {
-      const NAME = isValid(item[keys[0]])
-        ? item[keys[0]].replace(/[0-9]/g, '')
-        : item[keys[0]]
-            .replace(/[^a-zA-Z ]/g, '')
-            .replace(/[0-9]/g, '');
-
       const sql = `INSERT INTO poc_config.season_${
         file.split('.')[0]
-      } (${rowNames}) VALUES ('${NAME}', '${item[keys[1]]}', '${
-        item[keys[2]]
-      }', '${item[keys[3]]}', '${item[keys[4]]}', '${
-        item[keys[5]]
-      }', '${item[keys[6]]}', '${item[keys[7]]}', '${
-        item[keys[8]]
-      }', '${item[keys[9]]}', '${item[keys[10]]}', '${
-        item[keys[11]]
-      }', '${item[keys[12]]}', '${item[keys[13]]}');`;
+      } (${rowNames}) VALUES ('${item[keys[0]]}', '${
+        item[keys[1]]
+      }', '${item[keys[2]]}', '${item[keys[3]]}', '${
+        item[keys[4]]
+      }', '${item[keys[5]]}', '${item[keys[6]]}', '${
+        item[keys[7]]
+      }', '${item[keys[8]]}', '${item[keys[9]]}', '${
+        item[keys[10]]
+      }', '${item[keys[11]]}', '${item[keys[12]]}', '${
+        item[keys[13]]
+      }');`;
 
       console.log(sql);
       query(sql);
