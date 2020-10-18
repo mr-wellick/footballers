84ff395edcbc76a59b99f760dee133a3f0323395
mr-wellick <lorenz3192@gmail.com>
2020-10-17 20:10:15 -0700

    refactor

--- src/create-db.js
+++ src/create-db.js
@@ -45,77 +45,54 @@ export const dropTable = async () => {
 export const createTable = async () => {
   const files = await getFileNames();
 
-  for (const file of files) {
-    const contents = await readFile(
-      path.join(__dirname, `../src/data/${file}`),
-      'utf8'
-    );
-    const parsedData = csvParse(contents);
-    let rows = '';
-
-    Object.keys(parsedData[0])
-      .filter(key => key !== 'num')
-      .forEach(key => {
-        if (key === 'rc') {
-          rows += `${key} varchar(255)\n`;
-        } else {
-          rows += `${key} varchar(255),\n`;
-        }
-      });
-
-    let sql = `CREATE TABLE poc_config.season_${
-      file.split('.')[0]
-    }(${rows});`;
-
-    console.log('');
-    console.log(chalk.cyan('---- CREATING TABLE ----'));
-    console.log(sql);
-    query(sql);
-    console.log('');
+  try {
+    for (const file of files) {
+      const contents = await readFile(
+        path.join(__dirname, `../src/data/${file}`),
+        'utf8'
+      );
+      const parsedData = csvParse(contents);
+      const rows = parsedData['columns']
+        .map(row => `${row} VARCHAR(255)`)
+        .join(', ');
+      const sql = `CREATE TABLE poc_config.season_${
+        file.split('.')[0]
+      }(${rows});`;
+      console.log(chalk.cyan('  CREATING TABLE'), '===>', sql);
+      query(sql);
+    }
+  } catch (err) {
+    console.error('Cannot create table', err);
+    return;
   }
 };
 
 export const insertIntoTable = async () => {
   const files = await getFileNames();
-  for (const file of files) {
-    const contents = await readFile(
-      path.join(__dirname, `../src/data/${file}`),
-      'utf8'
-    );
-    const parsedData = csvParse(contents);
-    const keys = Object.keys(parsedData[0]).filter(
-      key => key !== 'num'
-    );
-    let rowNames = '';
 
-    Object.keys(parsedData[0])
-      .filter(key => key !== 'num')
-      .forEach(key => {
-        if (key === 'rc') {
-          rowNames += `${key}`;
-        } else {
-          rowNames += `${key}, `;
-        }
+  try {
+    for (const file of files) {
+      const contents = await readFile(
+        path.join(__dirname, `../src/data/${file}`),
+        'utf8'
+      );
+      const parsedData = csvParse(contents);
+      const rows = parsedData['columns'].join(', ');
+
+      parsedData.forEach(item => {
+        const values = Object.values(item)
+          .map(value => `'${value}'`)
+          .join(', ');
+        const sql = `INSERT INTO poc_config.season_${
+          file.split('.')[0]
+        }(${rows}) VALUES (${values});`;
+        console.log(sql);
+        query(sql);
       });
-
-    console.log(chalk.cyan('---- INSER INTO TABLE ----'));
-    parsedData.forEach(item => {
-      const sql = `INSERT INTO poc_config.season_${
-        file.split('.')[0]
-      } (${rowNames}) VALUES ('${item[keys[0]]}', '${
-        item[keys[1]]
-      }', '${item[keys[2]]}', '${item[keys[3]]}', '${
-        item[keys[4]]
-      }', '${item[keys[5]]}', '${item[keys[6]]}', '${
-        item[keys[7]]
-      }', '${item[keys[8]]}', '${item[keys[9]]}', '${
-        item[keys[10]]
-      }', '${item[keys[11]]}', '${item[keys[12]]}', '${
-        item[keys[13]]
-      }');`;
-
-      console.log(sql);
-      query(sql);
-    });
+    }
+    return;
+  } catch (err) {
+    console.err('Cannot insert values into table', err);
+    return;
   }
 };
