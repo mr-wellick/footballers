5353802a56d4ee990095bf6aa9e5475d6c6cdadc
mr-wellick <lorenz3192@gmail.com>
2020-10-26 12:26:51 -0700

    change comments in test cases

--- src/utilities/__tests__/promise.util.js
+++ src/utilities/__tests__/promise.util.js
@@ -1,6 +1,6 @@
 import promiseUtil from '../promise.util.js';
 
-test('after a successful api call, err should be null and data should be a valid value to work with', async () => {
+test('after a successful api call, err should be undefined and data should be a valid value to work with', async () => {
   const [err, data] = await promiseUtil(
     new Promise((resolve) => {
       resolve('this is my response data');
@@ -11,7 +11,7 @@ test('after a successful api call, err should be null and data should be a valid
   expect(data).toBe('this is my response data');
 });
 
-test('after an unsuccessful api call, err should not be null data should be undefined', async () => {
+test('after an unsuccessful api call, err should be a valid value to work with and data should be undefined', async () => {
   const [err, data] = await promiseUtil(
     new Promise((resolve, reject) => {
       reject('this is my error');
