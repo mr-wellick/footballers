d3e56a2971eb1bfc3550f1cab5c50cd0a16ee6a3
mr-wellick <lorenz3192@gmail.com>
2020-11-15 23:31:41 -0800

    update user controllers unit tests

--- src/users/__tests__/user.controllers.js
+++ src/users/__tests__/user.controllers.js
@@ -2,62 +2,86 @@ import app from '../../index.js';
 import request from 'supertest';
 
 test('validateInput middleware case: body is missing', () => {
+  const route = '/api/v1/user/login';
+  const body = {};
+
   return new Promise((done) => {
     request(app)
-      .post('/api/v1/user/login')
-      .send({})
+      .post(route)
+      .send(body)
       .set('Accept', 'application/json')
-      .then((response) => {
-        expect(response.statusCode).toBe(400);
-        expect(response.body.errors.length).toBe(2);
+      .expect('Content-Type', /json/)
+      .then((res) => {
+        const { statusCode, body } = res;
+
+        expect(statusCode).toBe(400);
+        expect(body.errors.length).toBe(2);
         done();
       });
   });
 });
 
 test('userLogin controller case: user does not exist', () => {
+  const route = '/api/v1/user/login';
+  const body = {
+    user_email: 'test@test.com',
+    user_password: '12345678',
+  };
+
   return new Promise((done) => {
     request(app)
-      .post('/api/v1/user/login')
-      .send({
-        user_email: 'test@test.com',
-        user_password: '12345678',
-      })
+      .post(route)
+      .send(body)
       .set('Accept', 'application/json')
-      .then((response) => {
-        expect(response.statusCode).toBe(404);
+      // .expect('Content-Type', /json/)
+      .then((res) => {
+        const { statusCode } = res;
+
+        expect(statusCode).toBe(404);
         done();
       });
   });
 });
 
 test('userLogin controller case: wrong password', () => {
+  const route = '/api/v1/user/login';
+  const body = {
+    user_email: 'somename@gmail.com',
+    user_password: '123456789',
+  };
+
   return new Promise((done) => {
     request(app)
-      .post('/api/v1/user/login')
-      .send({
-        user_email: 'somename@gmail.com',
-        user_password: '123456789',
-      })
+      .post(route)
+      .send(body)
       .set('Accept', 'application/json')
-      .then((response) => {
-        expect(response.statusCode).toBe(401);
+      // .expect('Content-Type', /json/)
+      .then((res) => {
+        const { statusCode } = res;
+
+        expect(statusCode).toBe(401);
         done();
       });
   });
 });
 
 test('userLogin controller case: successful login', () => {
+  const route = '/api/v1/user/login';
+  const body = {
+    user_email: 'somename@gmail.com',
+    user_password: '12345678',
+  };
+
   return new Promise((done) => {
     request(app)
-      .post('/api/v1/user/login')
-      .send({
-        user_email: 'somename@gmail.com',
-        user_password: '12345678',
-      })
+      .post(route)
+      .send(body)
       .set('Accept', 'application/json')
-      .then((response) => {
-        expect(response.statusCode).toBe(200);
+      // .expect('Content-Type', /json/)
+      .then((res) => {
+        const { statusCode } = res;
+
+        expect(statusCode).toBe(200);
         done();
       });
   });
