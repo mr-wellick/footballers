d702cfe41d294a6feb98530f4721d367f31a10a9
mr-wellick <lorenz3192@gmail.com>
2020-12-03 21:53:58 -0800

    testing session management

--- src/app.js
+++ src/app.js
@@ -4,11 +4,30 @@ import { json } from 'body-parser';
 import { urlencoded } from 'body-parser';
 import morgan from 'morgan';
 import { config } from 'dotenv';
+import session from 'express-session';
+import redis from 'redis';
+import connectRedis from 'connect-redis';
 import { userRouter } from './services';
 
 // app initialization
 config();
 const app = express();
+const redisStore = connectRedis(session);
+const redisClient = redis.createClient();
+
+// add session management
+app.use(
+  session({
+    secret: 'hit',
+    store: new redisStore({
+      host: 'localhost',
+      port: 6379,
+      client: redisClient,
+    }),
+    saveUninitialized: false,
+    resave: false,
+  })
+);
 
 // add base middleware
 app.use(json());
