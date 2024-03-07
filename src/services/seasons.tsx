567121796059fc94d0f76cf96534d5757bc84699
mr-wellick <lorenz3192@gmail.com>
2024-03-06 21:10:46 -0800

    data is now an array as expected

--- src/services/seasons.tsx
+++ src/services/seasons.tsx
@@ -5,14 +5,13 @@ import Seasons from '../views/seasons';
 const app = new Hono();
 
 app.get('/seasons', async (c) => {
-  // only getting back on item in data???
   const { data, error } = await supabase.rpc('get_all_tables');
 
   if (error) {
     return c.html('You don goofed');
   }
 
-  return c.html(<Seasons data={[data]} />);
+  return c.html(<Seasons data={data} />);
 });
 
 export default app;
